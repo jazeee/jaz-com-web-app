1e20e0e09b42bc18743000b60cafd924dab6680a
Jaz Singh <jazeee@gmail.com>
2024-05-05 11:54:10 -0700

    Add reading api code and types

---
+++ src/Readings/api.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { SummarizedPlotDatum } from "./types";
+import constate from "constate";
+
+function useReadings() {
+  const readingsResponse = useQuery({
+    queryKey: ["readings"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:4242/readings");
+      const readings: { readings: SummarizedPlotDatum[] } =
+        await response.json();
+      return readings;
+    },
+    refetchInterval: 1 * 60 * 1000,
+    refetchIntervalInBackground: true,
+    staleTime: 0.5 * 60 * 1000,
+  });
+  const { data, isSuccess } = readingsResponse;
+  const latestReading = isSuccess ? data.readings[0] : undefined;
+  const latestReadingValue = latestReading?.value;
+  return { readingsResponse, latestReading, latestReadingValue };
+}
+
+export const [ReadingsProvider, useReadingsContext] = constate(useReadings);
---
+++ src/Readings/types.ts
@@ -0,0 +1,46 @@
+export interface PlotDimensions {
+  plotWidth: number;
+  plotHeight: number;
+}
+
+export enum Trend {
+  DoubleDown = "DoubleDown",
+  SingleDown = "SingleDown",
+  FortyFiveDown = "FortyFiveDown",
+  Flat = "Flat",
+  FortyFiveUp = "FortyFiveUp",
+  SingleUp = "SingleUp",
+  DoubleUp = "DoubleUp",
+}
+
+export interface PlotDatumDateProps {
+  timeInMilliseconds?: number;
+  timeInSeconds?: number;
+  timeSinceLastReadingInSeconds?: number;
+  timeSinceLastReadingInMinutes?: number;
+  readingIsOld?: boolean;
+  date?: Date;
+}
+
+export interface SummarizedPlotDatum extends PlotDatumDateProps {
+  value: number;
+  trend: Trend;
+  isHigh: boolean;
+  isLow: boolean;
+  isInRange: boolean;
+  color: string;
+  opacity?: number;
+  isProjected?: boolean;
+  projectedIndex?: number;
+}
+
+export interface PlotSettings {
+  minScale: number;
+  maxScale: number;
+  lowWarning: number;
+  highWarning: number;
+  lowAxis: number;
+  highAxis: number;
+  higherAxis: number;
+  units: string;
+}
